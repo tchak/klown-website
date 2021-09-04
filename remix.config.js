af19ae580dfbfdc9901242837049e00487613c11
Paul Chavard <github@paul.chavard.net>
2021-09-04 15:37:47 +0200

    remove vercel

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,6 @@ module.exports = {
   appDirectory: 'app',
   browserBuildDirectory: 'public/build',
   publicPath: '/build/',
-  // Vercel
-  // serverBuildDirectory: 'server/build',
-  // devServerPort: 8002,
-  // Netlify
   serverBuildDirectory: 'netlify/functions/server/build',
   devServerPort: 8888,
 };
