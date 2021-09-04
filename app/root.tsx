7ba62c55f7598aef3601a7b2ebb52c3199e6ac40
Paul Chavard <github@paul.chavard.net>
2021-09-04 21:02:30 +0200

    Add stack to error

--- app/root.tsx
+++ app/root.tsx
@@ -70,6 +70,7 @@ export function ErrorBoundary({ error }: { error: Error }) {
     <Document>
       <h1>App Error</h1>
       <pre>{error.message}</pre>
+      <pre>{error.stack}</pre>
       <p>
         Replace this UI with what you want users to see when your app throws
         uncaught errors.
