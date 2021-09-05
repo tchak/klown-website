33ecb127eea717838ec3cb91f540d74b93d7880e
Paul Chavard <github@paul.chavard.net>
2021-09-05 16:36:12 +0200

    more inlining

--- app/root.tsx
+++ app/root.tsx
@@ -56,12 +56,47 @@ function Document({
 }
 
 export default function App() {
+  const data = useRouteData<GetHeader>();
   return (
     <Document bodyId={useBodyId()}>
-      <Header />
+      <header id="topbar" role="navigation" aria-label="topbar">
+        <h1>
+          <Link to="/info">Klown</Link>
+        </h1>
+        <nav>
+          <span className="parent">Menu</span>
+          <ul>
+            {data.categories.map((category) => (
+              <li key={category.id}>
+                <NavLink to={`/category/${category.slug}`}>
+                  {category.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
       <Outlet />
       <Strobos />
-      <Footer />
+      <footer>
+        <p className="description">
+          Le site du Klown est un espace de présentation du travail d’Igor
+          Galligo. Ce travail est représenté par la gallerie XX à Paris.
+        </p>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/info">info</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <p className="mentions">2021 — Tous droits réservés</p>
+        <p className="mentions">Design Dev: </p>
+        <InlineSVG />
+      </footer>
     </Document>
   );
 }
@@ -77,53 +112,5 @@ export function ErrorBoundary({ error }: { error: Error }) {
         uncaught errors.
       </p>
     </Document>
-  );
-}
-
-export function Header() {
-  const data = useRouteData<GetHeader>();
-
-  return (
-    <header id="topbar" role="navigation" aria-label="topbar">
-      <h1>
-        <Link to="/info">Klown</Link>
-      </h1>
-      <nav>
-        <span className="parent">Menu</span>
-        <ul>
-          {data.categories.map((category) => (
-            <li key={category.id}>
-              <NavLink to={`/category/${category.slug}`}>
-                {category.title}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-export function Footer() {
-  return (
-    <footer>
-      <p className="description">
-        Le site du Klown est un espace de présentation du travail d’Igor
-        Galligo. Ce travail est représenté par la gallerie XX à Paris.
-      </p>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/info">info</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">contact</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <p className="mentions">2021 — Tous droits réservés</p>
-      <p className="mentions">Design Dev: </p>
-      <InlineSVG />
-    </footer>
   );
 }
