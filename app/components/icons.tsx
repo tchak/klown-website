95c529eae71a83e3451bed33614418f49d560d1c
B <benjmng@gmail.com>
2021-09-16 17:03:19 +0200

    SVG Icons Side

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -85,7 +85,7 @@ export function IconSide({ height = 24, width = 24 }: IconParams) {
       preserveAspectRatio="xMinYMid meet"
     >
       <g id="i-side" className="stroke">
-        <line x1="0" y1="0.5" x2="25" y2="0.5" />
+        <line x1="0" y1="1.5" x2="25" y2="1.5" />
         <line x1="11" y1="17.6" x2="49.2" y2="17.6" />
         <line x1="0" y1="34.7" x2="25" y2="34.7" />
         <polyline points="42.3,10.7 49.2,17.6 42.3,24.5" />
